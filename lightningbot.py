e850c3b0609167ffbad186a0df52c9ae8b6e2022
CHATEAU-RICHERD Augustin <gus3000spam@gmail.com>
2018-10-25 20:09:41 +0200

    Added Enum for direction, explicit end messages, movement verbosity and room name at start

--- lightningbot.py
+++ lightningbot.py
@@ -2,14 +2,40 @@ import random
 import requests
 import json
 import time
+from enum import IntEnum
 
 
 class Bot:
-    pass
+    def __init__(self, mode: str = 'test'):
+        self.api_handler: ApiHandler = ApiHandler(mode)
+
+    def move(self, current_turn: int) -> bool:
+        direction = Direction.DOWN  # use Direction.UP, Direction.DOWN, Direction.LEFT or Direction.RIGHT
+        # TODO fill in your code here
+
+        # stop right there
+        print('moving', direction.name)
+        move_result = self.api_handler.move(direction=direction, current_turn=current_turn)
+        if 'error' in move_result:
+            # print('error :', move_result)
+            dirs: dict = self.api_handler.get_directions(current_turn=current_turn)
+            if dirs['error'] == 201:
+                print('This bot is dead.')
+            elif dirs['error'] == 200:
+                print('You won !')
+            return False
+        return True
+
+
+class Direction(IntEnum):
+    INIT = -1
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
 
 
 class ApiHandler:
-
     def __init__(self, mode: str = 'test', base_url: str = 'https://lightningbot.tk/api/'):
         self.url: str = base_url
         self.nextMove: time = time.perf_counter()
@@ -26,7 +52,6 @@ class ApiHandler:
             response = requests.get(
                 '/'.join([self.url, 'connect', self.pseudo]))  # Ask the server a token to play the game
             connect = json.loads(response.text)  # Get the answer as a json to read it easily
-            print("connect :", connect)
             self.token = connect['token']
 
         else:
@@ -35,37 +60,32 @@ class ApiHandler:
             print("Not implemented yet.")  # TODO
             exit(0)
 
+        print('Connected to room', connect['name'], '! Waiting for info...')
         time.sleep(connect['wait'] / 1000)  # wait for the end of the connect phase
         self.info = json.loads(requests.get('/'.join([self.url, 'info', self.token])).text)  # fetch info
         self.nextMove = time.perf_counter() + (self.info['wait'] / 1000)
 
-    def get_directions(self, current_turn) -> dict:
+    def get_directions(self, current_turn: int) -> dict:
         res = requests.get('/'.join([self.url, 'directions', self.token, str(current_turn)]))
         return json.loads(res.text)
 
-    def move(self, direction, current_turn) -> dict:
+    def move(self, direction: Direction, current_turn: int) -> dict:
         if time.perf_counter() < self.nextMove:  # if we moved too soon
             time.sleep(self.nextMove - time.perf_counter())
 
-        res = requests.get('/'.join([self.url, 'move', self.token, str(direction), str(current_turn)]))
+        res = requests.get('/'.join([self.url, 'move', self.token, str(direction.value), str(current_turn)]))
         resd = json.loads(res.text)
         while 'error' in resd and resd['error'] == 3:  # this shouldn't happen, but you never know
             print('Still early ? debug info :', current_turn, self.nextMove, time.perf_counter(), resd)
             time.sleep(resd['wait'] / 1000)
-            res = requests.get('/'.join([self.url, 'move', self.token, str(direction), str(current_turn)]))
+            res = requests.get('/'.join([self.url, 'move', self.token, str(direction.value), str(current_turn)]))
             resd = json.loads(res.text)
         self.nextMove = time.perf_counter() + (resd['wait'] / 1000)
         return resd
 
 
-h: ApiHandler = ApiHandler()
-running = True
-turn = 1
-while running:
-    result = h.move(1, turn)  # Move the bot and store the result of the request in a variable
-    running = result['success']
-    print('turn:', turn, "\tresult:", result)
-    turn += 1
-
-# print(h.get_directions(turn)['description'])
-print(h.get_directions(turn))
+if __name__ == '__main__':
+    b: Bot = Bot()
+    turn = 1
+    while b.move(turn):
+        turn += 1
