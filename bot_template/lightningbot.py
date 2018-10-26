df7adb00ed1128b3d2de7ea6d8a94d1fcdae14c3
CHATEAU-RICHERD Augustin <gus3000spam@gmail.com>
2018-10-26 16:18:55 +0200

    reorganizing, correcting network bugs and implementing a few utility functions

---
+++ bot_template/lightningbot.py
@@ -0,0 +1,140 @@
+import sys
+import random
+import requests
+import json
+import time
+from enum import IntEnum
+
+
+class Direction(IntEnum):
+    DEAD = -2
+    INIT = -1
+    RIGHT = 0
+    DOWN = 1
+    LEFT = 2
+    UP = 3
+
+    @staticmethod
+    def opposite(direction):
+        if direction == Direction.RIGHT:
+            return Direction.LEFT
+        elif direction == Direction.LEFT:
+            return Direction.RIGHT
+        elif direction == Direction.DOWN:
+            return Direction.UP
+        elif direction == Direction.UP:
+            return Direction.DOWN
+
+    @staticmethod
+    def valid(direction):
+        return direction in [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+
+class Bot:
+    def __init__(self, mode: str = 'test'):
+        self.api_handler: ApiHandler = ApiHandler(mode)
+        self.pseudo = self.api_handler.pseudo
+
+    def move(self, current_turn: int, api_directions: dict, directions: dict) -> Direction:
+        return Direction.DOWN  # use Direction.UP, Direction.DOWN, Direction.LEFT or Direction.RIGHT
+
+    def send_move(self, current_turn: int) -> bool:
+        directions: dict = self.api_handler.get_directions(current_turn)
+        if 'error' in directions:
+            if directions['error'] == 201:
+                print('This bot is dead.')
+            elif directions['error'] == 200:
+                print('You won !')
+            else:
+                print('directions are all wrong !', file=sys.stderr)
+                print('exiting now, debug info :', '\ncurrent_turn', current_turn, '\ndirections', directions,
+                      file=sys.stderr)
+                exit(-1)
+            return False
+
+        direction = self.move(current_turn=current_turn, directions=Bot.better_directions(directions),
+                              api_directions=directions)
+
+        print('moving', direction.name)
+        move_result = self.api_handler.move(direction=direction, current_turn=current_turn)
+
+        return True
+
+    # Utility methods
+    @staticmethod
+    def better_directions(api_directions: dict) -> dict:
+        dirs = {}
+        directions = api_directions['directions']
+        try:
+            for d in directions:
+                dirs[d['pseudo']] = Direction(int(d['direction']))
+        except (TypeError, ValueError):
+            print('failed better_directions on :', directions, file=sys.stderr)
+            return None
+        return dirs
+
+
+class ApiHandler:
+    def __init__(self, mode: str = 'test', base_url: str = 'https://lightningbot.tk/api/'):
+        """Constructs an Lightningbot API handler. It handles connection, info and direction retrieving and move sending.
+
+        :param mode: either "test" or "ranked"
+        :param base_url: defaults to 'https://lightningbot.tk/api/', and shouldn't change, but you never know
+        """
+        self.url: str = base_url
+        self.nextMove: time = time.perf_counter()
+        self.pseudo: str
+        self.token: str
+        self.info: dict
+        self.directions_cache: dict = {}
+
+        if mode == 'test':
+            print('Init Test mode')
+            self.url += 'test'
+            # name + a random number so you can launch two bot to play alone.
+            # If only one bot is connected the game will not start.
+            self.pseudo = 'Python' + str(random.randint(0, 9999))
+            response = requests.get(
+                '/'.join([self.url, 'connect', self.pseudo]))  # Ask the server a token to play the game
+            connect = json.loads(response.text)  # Get the answer as a json to read it easily
+            self.token = connect['token']
+
+        else:
+            print('Init Ranked mode')
+            connect = None
+            print("Not implemented yet.")  # TODO
+            exit(0)
+
+        print('Connected ! Waiting for info... (' + str(connect['wait'] / 1000) + ' s)')
+
+        time.sleep(connect['wait'] / 1000)  # wait for the end of the connect phase
+        self.info = json.loads(requests.get('/'.join([self.url, 'info', self.token])).text)  # fetch info
+        print('Room name :', self.info['name'])
+        self.nextMove = time.perf_counter() + (self.info['wait'] / 1000)
+
+    def get_directions(self, current_turn: int, force=False) -> dict:
+        if not force and current_turn in self.directions_cache:
+            return self.directions_cache[current_turn]
+
+        res = requests.get('/'.join([self.url, 'directions', self.token, str(current_turn)]))
+        current_directions = json.loads(res.text)
+        while 'error' in current_directions and current_directions['error'] == 3:
+            time.sleep(current_directions['wait']/1000)
+            res = requests.get('/'.join([self.url, 'directions', self.token, str(current_turn)]))
+            current_directions = json.loads(res.text)
+
+        self.directions_cache[current_turn] = current_directions
+        return current_directions
+
+    def move(self, direction: Direction, current_turn: int) -> dict:
+        if time.perf_counter() < self.nextMove:  # if we moved too soon
+            time.sleep(self.nextMove - time.perf_counter())
+
+        res = requests.get('/'.join([self.url, 'move', self.token, str(direction.value), str(current_turn)]))
+        resd = json.loads(res.text)
+        while 'error' in resd and resd['error'] == 3:  # this shouldn't happen, but you never know
+            print('Still early ? debug info :', current_turn, self.nextMove, time.perf_counter(), resd)
+            time.sleep(resd['wait'] / 1000)
+            res = requests.get('/'.join([self.url, 'move', self.token, str(direction.value), str(current_turn)]))
+            resd = json.loads(res.text)
+        self.nextMove = time.perf_counter() + (resd['wait'] / 1000)
+        return resd
---
+++ mybot.py
@@ -0,0 +1,44 @@
+import random
+import requests
+import json
+import time
+from enum import IntEnum
+from bot_template.lightningbot import Direction, ApiHandler, Bot
+
+
+class MyBot(Bot):
+    """
+    Use this class to get started. Just fill in the move method
+    """
+
+    def __init__(self, mode: str = 'test'):
+        super(MyBot, self).__init__(mode)
+
+    def move(self, current_turn: int, api_directions: list, directions: dict) -> Direction:
+        """
+        :param current_turn: the current turn
+        :param api_directions: the bots' current directions (before moving), as given by the API
+        :param directions: the bots' current directions, as a dictionnary
+            ex : {'player1': Direction.DOWN, 'player2': Direction.RIGHT
+        :return: the wanted Direction
+        """
+        # TODO fill in your code here
+        # use Direction.UP, Direction.DOWN, Direction.LEFT or Direction.RIGHT
+        # for example go down :
+        direction = Direction.DOWN
+
+        # or choose a random direction (spoiler : it will die quickly):
+        # direction = random.choice([d for d in Direction])
+
+        # or choose a random direction expect the opposite one :
+        # direction = random.choice([d for d in Direction if Direction.valid(d)
+        # and d != Direction.opposite(directions[self.pseudo])])
+
+        return direction
+
+
+if __name__ == '__main__':
+    b: Bot = MyBot()
+    turn = 1
+    while b.send_move(turn):
+        turn += 1
